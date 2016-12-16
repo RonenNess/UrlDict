6be41258b70bd624d0b9b4f25285878d1a79491a
Ronen <RonenNess@gmail.com>
2016-12-16 15:14:42 +0200

    finished tests and updated readme

--- README.md
+++ README.md
@@ -25,34 +25,34 @@ Or simply download the js files from /dist/ in this repo.
 Lets start with a simple example that covers most of it:
 
 ```JavaScript
-	// create a new urlDict object
-	var urlDict = new UrlDict("www.example.com");
-	
-	// set url GET param
-	urlDict.GET.set("a", 123);
-	urlDict.GET.set("b", 456);
-	
-	// build url. output will be "www.example.com?a=123&b=456"
-	alert(urlDict.toUrl());
-	
-	// now set hash param
-	urlDict.HASH.set("foo", "bar");
-	
-	// and now url output will be "www.example.com?a=123&b=456#foo=bar"
-	alert(urlDict.toUrl());
+// create a new urlDict object
+var urlDict = new UrlDict("www.example.com");
+
+// set url GET param
+urlDict.GET.set("a", 123);
+urlDict.GET.set("b", 456);
+
+// build url. output will be "www.example.com?a=123&b=456"
+alert(urlDict.toUrl());
+
+// now set hash param
+urlDict.HASH.set("foo", "bar");
+
+// and now url output will be "www.example.com?a=123&b=456#foo=bar"
+alert(urlDict.toUrl());
 ```
 
 Or if you want to get params from existing url:
 
 ```JavaScript
-	// build new url dict from url with GET and HASH params
-	var urlDict = new UrlDict("www.example.com?a=123&b=456#foo=bar");
-	
-	// will output "123":
-	alert(urlDict.GET.get("a");
-	
-	// will output "bar"
-	alert(urlDict.HASH.get("foo");
+// build new url dict from url with GET and HASH params
+var urlDict = new UrlDict("www.example.com?a=123&b=456#foo=bar");
+
+// will output "123":
+alert(urlDict.GET.get("a");
+
+// will output "bar"
+alert(urlDict.HASH.get("foo");
 ```
 
 Note that UrlDict try to parse params using the JSON parser, which means that values like ```a=true``` will be returned as a boolean, and ```b=123``` as a number. However, there's a special exception that if the value is not a valid JSON string it will just treat it as string.
@@ -62,40 +62,40 @@ Note that UrlDict try to parse params using the JSON parser, which means that va
 To create a new UrlDict object:
 
 ```JavaScript
-	// if 'url' is not provided, will use location.href instead.
-	var urlDict = new UrlDict(url);
+// if 'url' is not provided, will use location.href instead.
+var urlDict = new UrlDict(url);
 ```
 
 To build the full URL from UrlDict, use ```toUrl()```:
 
 ```JavaScript
-	var url = urlDict.toUrl();
+var url = urlDict.toUrl();
 ```
 
 To get or set just the base URL, without the GET or Hash params, use ```getBaseUrl()``` and ```setBaseUrl()```:
 
 ```JavaScript
-	// set the base url, eg the part before the GET and hash params:
-	urlDict.setBaseUrl(newBaseUrl);
-	
-	// get the base url, eg the part before the GET and hash params:
-	var baseUrl = urlDict.getBaseUrl();
+// set the base url, eg the part before the GET and hash params:
+urlDict.setBaseUrl(newBaseUrl);
+
+// get the base url, eg the part before the GET and hash params:
+var baseUrl = urlDict.getBaseUrl();
 ```
 
 To get a dictionary containing both GET and HASH params (objects are mutable and changing them will affect the urlDict), use ```asDict()```:
 
 ```JavaScript
-	// get params as dictionary
-	var params = urlDict.asDict();
-	
-	// show GET params
-	console.log(params.GET);
-	
-	// show HASH params
-	console.log(params.HASH);
-	
-	// to add a new GET param to url
-	params.GET["a"] = 123;
+// get params as dictionary
+var params = urlDict.asDict();
+
+// show GET params
+console.log(params.GET);
+
+// show HASH params
+console.log(params.HASH);
+
+// to add a new GET param to url
+params.GET["a"] = 123;
 ```
 
 #### GET and HASH
@@ -103,17 +103,17 @@ To get a dictionary containing both GET and HASH params (objects are mutable and
 The main API to set / get URL params is via the ```GET``` and ```HASH``` objects. They share the same API:
 
 ```JavaScript
-	// set url GET param
-	urlDict.GET.set("a", 123);
-	
-	// get url GET param
-	var a = urlDict.GET.get("a");
-	
-	// remove url GET param
-	urlDict.GET.remove("a");
-	
-	// clear all url GET params
-	urlDict.GET.clear();
+// set url GET param
+urlDict.GET.set("a", 123);
+
+// get url GET param
+var a = urlDict.GET.get("a");
+
+// remove url GET param
+urlDict.GET.remove("a");
+
+// clear all url GET params
+urlDict.GET.clear();
 ```
 
 The API above is the same for HASH object.
@@ -121,11 +121,11 @@ The API above is the same for HASH object.
 Or you can just get all params as a mutable dictionary (changing this dictionary will change the urlDict object):
 
 ```JavaScript
-	// get all url GET params as a dictionary:
-	var getParams = urlDict.GET.asDict();
-	
-	// change a url GET param
-	getParams["a"] = 123;
+// get all url GET params as a dictionary:
+var getParams = urlDict.GET.asDict();
+
+// change a url GET param
+getParams["a"] = 123;
 ```
 
 ## Tests & Browser support
--- tests/test.js
+++ tests/test.js
@@ -58,11 +58,193 @@ var covered = set_coverage(UrlDict);
 QUnit.test( "basic creation", function( assert ) {
 
 	// build with default current url
-	var urlDict = new UrlDict();
 	if (typeof location !== "undefined") {
+		var urlDict = new UrlDict();
 		assert.equal(urlDict.toUrl(), location.href);
 	}
   
+	// build with starting url
+	var urlDict = new UrlDict("www.test.com");
+	assert.equal(urlDict.getBaseUrl(), "www.test.com");
+	assert.equal(urlDict.toUrl(), "www.test.com");
+});
+
+// parse params from url
+QUnit.test( "parse params", function( assert ) {
+
+	// build from url with lots of params and check the output dictionary
+	var urlDict = new UrlDict('www.test.com?a="123"&b=bla&c=true&d=123#a=1&b="bla"&c=false&d=a1');
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1');
+	assert.deepEqual(urlDict.asDict(), {
+		GET: {
+			a: "123",
+			b: "bla",
+			c: true,
+			d: 123,
+		},
+		HASH: {
+			a: 1,
+			b: "bla",
+			c: false,
+			d: "a1",
+		}
+	});
+	
+	// now chage few params
+	urlDict.GET.set("a", false);
+	urlDict.HASH.set("aaa", "bbb");
+	
+	// make sure url is properly built
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=false&b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1&aaa=bbb');
+});
+
+// using as dictionary
+QUnit.test( "using as dict", function( assert ) {
+
+	// build with starting url
+	var urlDict = new UrlDict("www.test.com");
+	
+	// use as dictionary
+	urlDict.asDict().GET["a"] = 123;
+	urlDict.asDict().GET["b"] = true;
+	urlDict.asDict().HASH["foo"] = "bar";
+	urlDict.asDict().HASH["go"] = "abc";
+	
+	// convert to url
+	assert.equal(urlDict.toUrl(), "www.test.com?a=123&b=true#foo=bar&go=abc");
+});
+
+// using the GET api
+QUnit.test( "GET api", function( assert ) {
+
+	// create url with params 
+	var urlDict = new UrlDict('www.test.com?a="123"&b=bla&c=true&d=123#a=1&b="bla"&c=false&d=a1');
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1');
+	
+	// make sure GET as dict is correct
+	assert.deepEqual(urlDict.GET.asDict(), {
+		a: "123",
+		b: "bla",
+		c: true,
+		d: 123,
+	});
+	
+	// set url GET param
+    urlDict.GET.set("a", 123);
+	assert.deepEqual(urlDict.GET.asDict(), {
+		a: 123,
+		b: "bla",
+		c: true,
+		d: 123,
+	});
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1');
+
+    // get url GET param
+    var a = urlDict.GET.get("a");
+	assert.strictEqual(a, 123);
+
+    // remove url GET param
+    urlDict.GET.remove("a");
+	assert.deepEqual(urlDict.GET.asDict(), {
+		b: "bla",
+		c: true,
+		d: 123,
+	});
+	var a = urlDict.GET.get("a");
+	assert.strictEqual(a, undefined);
+	assert.equal(urlDict.toUrl(), 'www.test.com?b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1');
+
+    // clear all url GET params
+    urlDict.GET.clear();
+	assert.equal(urlDict.toUrl(), 'www.test.com#a=1&b=bla&c=false&d=a1');
+	assert.deepEqual(urlDict.GET.asDict(), {});
+});
+
+// using the HASH api
+QUnit.test( "HASH api", function( assert ) {
+
+	// create url with params 
+	var urlDict = new UrlDict('www.test.com?a="123"&b=bla&c=true&d=123#a=1&b="bla"&c=false&d=a1');
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#a=1&b=bla&c=false&d=a1');
+	
+	// make sure HASH as dict is correct
+	assert.deepEqual(urlDict.HASH.asDict(), {
+		a: 1,
+		b: "bla",
+		c: false,
+		d: "a1",
+	});
+	
+	// set url HASH param
+    urlDict.HASH.set("a", 123);
+	assert.deepEqual(urlDict.HASH.asDict(), {
+		a: 123,
+		b: "bla",
+		c: false,
+		d: "a1",
+	});
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#a=123&b=bla&c=false&d=a1');
+
+    // get url HASH param
+    var a = urlDict.HASH.get("a");
+	assert.strictEqual(a, 123);
+
+    // remove url HASH param
+    urlDict.HASH.remove("a");
+	assert.deepEqual(urlDict.HASH.asDict(), {
+		b: "bla",
+		c: false,
+		d: "a1",
+	});
+	var a = urlDict.HASH.get("a");
+	assert.strictEqual(a, undefined);
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123#b=bla&c=false&d=a1');
+
+    // clear all url HASH params
+    urlDict.HASH.clear();
+	assert.equal(urlDict.toUrl(), 'www.test.com?a=123&b=bla&c=true&d=123');
+	assert.deepEqual(urlDict.HASH.asDict(), {});
+});
+
+// using clear all
+QUnit.test( "clear all", function( assert ) {
+
+	// build with starting url
+	var urlDict = new UrlDict("www.test.com");
+	
+	// use as dictionary
+	urlDict.asDict().GET["a"] = 123;
+	urlDict.asDict().GET["b"] = true;
+	urlDict.asDict().HASH["foo"] = "bar";
+	urlDict.asDict().HASH["go"] = "abc";
+	
+	// convert to url
+	assert.equal(urlDict.toUrl(), "www.test.com?a=123&b=true#foo=bar&go=abc");
+	
+	// now clear all and make sure it no longer have params
+	urlDict.clearParams();
+	assert.equal(urlDict.toUrl(), "www.test.com");
+	assert.deepEqual(urlDict.asDict(), {GET: {}, HASH: {}});
+});
+
+// set / get base url
+QUnit.test( "base url", function( assert ) {
+
+	// build with starting url
+	var urlDict = new UrlDict("www.test.com");
+	
+	// use as dictionary
+	urlDict.asDict().GET["a"] = 123;
+	urlDict.asDict().HASH["foo"] = "bar";
+	
+	// get base url
+	assert.equal(urlDict.getBaseUrl(), "www.test.com");
+	assert.equal(urlDict.toUrl(), "www.test.com?a=123#foo=bar");
+	
+	// set base url
+	urlDict.setBaseUrl("www.foo.bar")
+	assert.equal(urlDict.getBaseUrl(), "www.foo.bar");
+	assert.equal(urlDict.toUrl(), "www.foo.bar?a=123#foo=bar");
 });
 
 // testing the first example from the readme file
